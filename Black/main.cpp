4f2ef0a2368e94747704184bee1c4c741ab917d4
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2022-08-21 21:02:57 +0530

    Initial Commit

---
+++ Black/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Black
+//
+//  Created by Apekshik Panigrahi on 8/21/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
